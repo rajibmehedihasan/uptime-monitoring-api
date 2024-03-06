1aaa3393cf0cb3c67ec56a44a088cf9efdb5830c
rajibmehedihasan <mehedi.hasan2724@gmail.com>
2024-03-06 23:25:37 +0600

    add all methods

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -60,4 +60,10 @@ handler._users.post = (requestProperties, callback) => {
     }
 };
 
+handler._users.get = (requestProperties, callback) => {};
+
+handler._users.put = (requestProperties, callback) => {};
+
+handler._users.delete = (requestProperties, callback) => {};
+
 module.exports = handler;
