153a4d2c5074e47ccc36c0e83afd4638fc21d751
rajibmehedihasan <mehedi.hasan2724@gmail.com>
2024-03-21 03:45:10 +0600

    add get token functionality

--- handlers/routeHandlers/tokenHandler.js
+++ handlers/routeHandlers/tokenHandler.js
@@ -79,4 +79,174 @@ handler._token.post = (requestProperties, callback) => {
     }
 };
 
+handler._token.get = (requestProperties, callback) => {
+    const id = stringValidator(requestProperties.queryStringObject.get("id"));
+
+    if (!id) {
+        callback(404, {
+            error: "Invalid Request",
+        });
+        return;
+    }
+
+    read("tokens", id, (err, tokenData) => {
+        const token = { ...parseJSON(tokenData) };
+        if (err || !token) {
+            callback(404, {
+                error: "Requested user not found!",
+            });
+            return;
+        }
+
+        callback(200, token);
+    });
+};
+
+handler._token.put = (requestProperties, callback) => {
+    const { phone, password } = requestProperties.body;
+    const _phone = phoneStringValidator(phone);
+    const _password = stringValidator(password);
+
+    if (_phone && _password) {
+        read("users", _phone, (err, userData) => {
+            if (err || !userData) {
+                callback(400, {
+                    error: "Invalid phone number provided.",
+                });
+                return;
+            }
+
+            const _userData = parseJSON(userData);
+            const hashPassword = hash(password, phone);
+
+            if (hashPassword === _userData.password) {
+                const tokenID = createRandomString(20);
+                const expires = Date.now() + 60 * 60 * 1000;
+                const tokenObject = {
+                    phone,
+                    id: tokenID,
+                    expires,
+                };
+
+                create("tokens", tokenID, tokenObject, (err2) => {
+                    if (err2) {
+                        callback(500, {
+                            error: "There was a problem in the server side!",
+                        });
+                        return;
+                    }
+
+                    callback(200, tokenObject);
+                });
+            } else {
+                callback(400, {
+                    error: "Password not valid!",
+                });
+            }
+        });
+    } else {
+        callback(400, {
+            error: "You have a problem in your request",
+        });
+    }
+};
+
+handler._token.delete = (requestProperties, callback) => {
+    const { phone, password } = requestProperties.body;
+    const _phone = phoneStringValidator(phone);
+    const _password = stringValidator(password);
+
+    if (_phone && _password) {
+        read("users", _phone, (err, userData) => {
+            if (err || !userData) {
+                callback(400, {
+                    error: "Invalid phone number provided.",
+                });
+                return;
+            }
+
+            const _userData = parseJSON(userData);
+            const hashPassword = hash(password, phone);
+
+            if (hashPassword === _userData.password) {
+                const tokenID = createRandomString(20);
+                const expires = Date.now() + 60 * 60 * 1000;
+                const tokenObject = {
+                    phone,
+                    id: tokenID,
+                    expires,
+                };
+
+                create("tokens", tokenID, tokenObject, (err2) => {
+                    if (err2) {
+                        callback(500, {
+                            error: "There was a problem in the server side!",
+                        });
+                        return;
+                    }
+
+                    callback(200, tokenObject);
+                });
+            } else {
+                callback(400, {
+                    error: "Password not valid!",
+                });
+            }
+        });
+    } else {
+        callback(400, {
+            error: "You have a problem in your request",
+        });
+    }
+};
+
+handler._token.verify = (requestProperties, callback) => {
+    const { phone, password } = requestProperties.body;
+    const _phone = phoneStringValidator(phone);
+    const _password = stringValidator(password);
+
+    if (_phone && _password) {
+        read("users", _phone, (err, userData) => {
+            if (err || !userData) {
+                callback(400, {
+                    error: "Invalid phone number provided.",
+                });
+                return;
+            }
+
+            const _userData = parseJSON(userData);
+            const hashPassword = hash(password, phone);
+
+            if (hashPassword === _userData.password) {
+                const tokenID = createRandomString(20);
+                const expires = Date.now() + 60 * 60 * 1000;
+                const tokenObject = {
+                    phone,
+                    id: tokenID,
+                    expires,
+                };
+
+                create("tokens", tokenID, tokenObject, (err2) => {
+                    if (err2) {
+                        callback(500, {
+                            error: "There was a problem in the server side!",
+                        });
+                        return;
+                    }
+
+                    callback(200, tokenObject);
+                });
+            } else {
+                callback(400, {
+                    error: "Password not valid!",
+                });
+            }
+        });
+    } else {
+        callback(400, {
+            error: "You have a problem in your request",
+        });
+    }
+};
+
 module.exports = handler;
